837c5daf413bdc310d7a00aa5c6a59152ffb6207
HotBarbeque <122079759+HotBarbeque@users.noreply.github.com>
2023-08-03 19:13:34 -0500

    Sign up styling

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -31,59 +31,125 @@ const SignUp = () => {
   };
 
   return (
-    <div className="bg-gray-300 h-screen flex flex-col items-center justify-center">
-      <h2 className="	flex justify-center bg-gray-300 text-white text-4xl py-10">Sign Up</h2>
-      <form className="flex flex-col bg-white items-center justify-center space-between w-full h-[800px]" onSubmit={handleSignUp}>
-        <div className="w-full h-full flex flex-col justify-center items-center space-around">
-        <div className="bg-white text-left">
-          <label className="text-left" htmlFor="firstName">First Name:</label>
-          <input className="bg-gray-100"
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <form className="
+          flex flex-col
+          bg-gray-100
+          shadow-md
+          px-4
+          sm:px-6
+          md:px-8
+          lg:px-10
+          py-8
+          w-50
+          max-w-md
+        " onSubmit={handleSignUp}>
+        <div className="font-medium self-center text-xl sm:text-3xl text-gray-800">Sign up
+        </div>
+        <div className="mt-10">
+        <div className="flex flex-col mb-5">
+          <label className="mb-1 text-xs tracking-wide text-gray-600" htmlFor="firstName">First Name:</label>
+          <input className="text-sm
+                    placeholder-gray-500
+                    pl-10
+                    pr-4
+                    border border-gray-400
+                    w-full
+                    py-2
+                    focus:outline-none focus:border-red-400"
+            placeholder="First Name"
             type="text"
             id="firstName"
             value={firstName}
             onChange={(e) => setFirstName(e.target.value)}
           />
         </div>
-        <div className="bg-white space-between">
-          <label className="text-left" htmlFor="lastName">Last Name:</label>
-          <input className="bg-gray-100"
+        <div className="flex flex-col mb-5">
+          <label className="mb-1 text-xs tracking-wide text-gray-600" htmlFor="lastName">Last Name:</label>
+          <input className="text-sm
+                    placeholder-gray-500
+                    pl-10
+                    pr-4
+                    border border-gray-400
+                    w-full
+                    py-2
+                    focus:outline-none focus:border-red-400"
+            placeholder="Last Name"
             type="text"
             id="lastName"
             value={lastName}
             onChange={(e) => setLastName(e.target.value)}
           />
         </div>
-        <div className="bg-white w-100 space-between">
-          <label htmlFor="email">Email:</label>
-          <input className="bg-gray-100"
+        <div className="flex flex-col mb-5">
+          <label className="mb-1 text-xs tracking-wide text-gray-600" htmlFor="email">Email:</label>
+          <input className="text-sm
+                    placeholder-gray-500
+                    pl-10
+                    pr-4
+                    border border-gray-400
+                    w-full
+                    py-2
+                    focus:outline-none focus:border-red-400"
+            placeholder="Email"
             type="email"
             id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
         </div>
-        <div className="bg-white">
-          <label htmlFor="username">Username:</label>
-          <input className="bg-gray-100"
+        <div className="flex flex-col mb-5">
+          <label className="mb-1 text-xs tracking-wide text-gray-600" htmlFor="username">Username:</label>
+          <input className="text-sm
+                    placeholder-gray-500
+                    pl-10
+                    pr-4
+                    border border-gray-400
+                    w-full
+                    py-2
+                    focus:outline-none focus:border-red-400"
+            placeholder="Username"
             type="text"
             id="username"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
         </div>
-        <div className="bg-white">
-          <label htmlFor="password">Password:</label>
-          <input className="bg-gray-100"
+        <div className="flex flex-col mb-5">
+          <label className="mb-1 text-xs tracking-wide text-gray-600" htmlFor="password">Password:</label>
+          <input className="text-sm
+                    placeholder-gray-500
+                    pl-10
+                    pr-4
+                    border border-gray-400
+                    w-full
+                    py-2
+                    focus:outline-none focus:border-red-400"
+            placeholder="Password"
             type="password"
             id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <button className="bg-red-500 text-white rounded-lg my-0.5 w-1/3" type="submit">Submit</button>
-      </div>
+        <button className="flex
+                  mt-2
+                  items-center
+                  justify-center
+                  focus:outline-none
+                  text-white text-sm
+                  sm:text-base
+                  bg-red-500
+                  hover:bg-red-600
+                  py-2
+                  w-full
+                  transition
+                  duration-150
+                  ease-in" type="submit">SUBMIT</button>
+        </div>
       </form>
     </div>
+    
   );
 };
 
