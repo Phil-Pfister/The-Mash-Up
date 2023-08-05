34d4768b24874477dd76bd11f1b38e723996e733
Phil-Pfister <salshouse@gmail.com>
2023-08-05 14:13:11 -0500

    one more little thing

--- client/src/pages/ProductForm.jsx
+++ client/src/pages/ProductForm.jsx
@@ -102,13 +102,13 @@ const ProductForm = () => {
       name='name' type="text" placeholder="name of product"/>
       
     </div>
-    {/* <div className="w-full md:w-1/2 px-3">
+    <div className="w-full md:w-1/2 px-3">
       <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-seller">
         Seller
       </label>
-      <input onChange={handleChange} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
-      name="seller" id="grid-last-name" type="text" placeholder="Username"/>
-    </div> */}
+      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+      name="seller" id="grid-last-name" type="text" value={Auth.getUser().data.username}/>
+    </div>
   </div>
   <div className="flex flex-wrap -mx-3 mb-6">
     <div className="w-full px-3">
